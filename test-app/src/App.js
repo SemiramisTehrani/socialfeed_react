22d6bde3d9c9d81eab13cfa22bca067b5f3d19de
unknown <semtehrani@yahoo.com>
2022-07-21 18:20:47 -0700

    Show casing Hello World! & I am from the name component! on server

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import NamePresenter from './components/NamePresenter';
 
 function App() {
   return (
     <div className="App">
       Hello World!
+      <NamePresenter />
     </div>
   );
 }
---
+++ test-app/src/components/NamePresenter.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NamePresenter = (props) => {
+
+    return <div> I am from the name component!</div>
+
+};
+
+export default NamePresenter;
